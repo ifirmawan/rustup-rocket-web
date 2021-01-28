c985bfdbb11623e220056231bee6d9ee65f0fcb6
iwanf <firmawaneiwan@gmail.com>
2021-01-28 08:11:59 +0700

    add dependency rocket and create hello routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket::response::content::Json;
+
+#[get("/hello")]
+fn hello() -> Json<&'static str> {
+  Json("{
+    'status': 'success',
+    'message': 'Hello API!'
+  }")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/api", routes![hello])
+        .launch();
 }
